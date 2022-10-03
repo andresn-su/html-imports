fe9db960cf9be7eff63fd9d35e31740e5ec5b343
André Nascimento <andre.s.04.get.up@gmail.com>
2022-10-03 13:05:46 -0300

    created the include tool

---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+ * Executes the scripts inside each component, marked with the 'inComponent' attribute
+ */
+function loadExtraScripts()
+{
+    let allScripts = Array.from(document.querySelectorAll('script[inComponent]'));
+    allScripts.forEach(script => {
+        eval(script.textContent);
+        script.remove();
+    });
+}
+
+/**
+ * Imports HTML components
+ * @returns void
+ */
+function importFiles()
+{
+    let req = new XMLHttpRequest;
+    let components = Array.from(document.querySelectorAll('app-include'));
+
+    if (!components.length) return;
+
+    // Importing and showing components
+    components.forEach(c => {
+        let src = c.getAttribute('src');
+        let file = src.split('.html').length === 2 ? src : src + ".html";
+        req.open("GET", file);
+        req.responseType = 'text/html';
+
+        // Shows the components
+        req.onload = () => {
+            if (req.readyState === req.DONE && req.status == 200) {
+                let div = document.createElement('div');
+                c.parentElement.insertBefore(div, c);
+                div.innerHTML = req.response;
+                c.remove();
+                
+                importFiles();
+                loadExtraScripts();
+            } else {
+                console.error("source not found");
+            }
+        }
+
+        req.send();
+    });
+}
+
+importFiles();
